df86331926079d651002e0daa9b01a6dbae5d246
Suhani Shah <suhanishah@live.com>
2021-03-29 08:56:08 -0400

    Initial Commit
    - MVC Project With Individual User Accounts

---
+++ Gig_Hub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Gig_Hub.Startup))]
+namespace Gig_Hub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
