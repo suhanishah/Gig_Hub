37d78a74e742bff5f19417d68aa1ce917a199dfd
Suhani Shah <suhanishah@live.com>
2021-03-29 11:08:09 -0400

    Add data Annotations

--- Gig_Hub/Models/Genre.cs
+++ Gig_Hub/Models/Genre.cs
@@ -1,8 +1,13 @@
-﻿namespace Gig_Hub.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gig_Hub.Models
 {
     public class Genre
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name{ get; set; }
     }
 }
