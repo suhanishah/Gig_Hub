9806a59d197ee75b4d1796dd1499e804c0e47ae6
Suhani Shah <suhanishah@live.com>
2021-03-29 13:46:01 -0400

    Add Gig Form

---
+++ Gig_Hub/Controllers/GigsController.cs
@@ -0,0 +1,27 @@
+﻿using Gig_Hub.Models;
+using Gig_Hub.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Gig_Hub.Content
+{
+    public class GigsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres =  _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ Gig_Hub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Gig_Hub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gig_Hub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+
+        public string Date { get; set; }
+
+        public string Time { get; set; }
+
+        public int Genre { get; set; }
+
+        public IEnumerable<Genre> Genres{ get; set; }
+    }
+}
